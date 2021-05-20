a2ab80b61bc821cf06aa3d3ab0988a9c00ac939a
PunkCatt <catharine.vetter@gmail.com>
2021-05-20 16:39:22 -0500

    to skjaald

--- skjaald/gulpfile.js
+++ skjaald/gulpfile.js
@@ -5,9 +5,9 @@ const less = require('gulp-less');
 /*  Compile LESS
 /* ----------------------------------------- */
 
-const DND5E_LESS = ["less/*.less"];
+const SKJAALD_LESS = ["less/*.less"];
 function compileLESS() {
-  return gulp.src("less/dnd5e.less")
+  return gulp.src("less/skjaald.less")
     .pipe(less())
     .pipe(gulp.dest("./"))
 }
@@ -18,7 +18,7 @@ const css = gulp.series(compileLESS);
 /* ----------------------------------------- */
 
 function watchUpdates() {
-  gulp.watch(DND5E_LESS, css);
+  gulp.watch(SKJAALD_LESS, css);
 }
 
 /* ----------------------------------------- */
